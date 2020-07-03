76f2a542d5e98089e28a586cec8ddd4102cce831
dds <11372904+ddstevenson@users.noreply.github.com>
2020-07-03 16:50:57 -0700

    Added PhoneBill class

---
+++ phonebill/src/main/java/edu/pdx/cs410J/ads6/PhoneBill.java
@@ -0,0 +1,23 @@
+package edu.pdx.cs410J.ads6;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+import java.util.Collection;
+
+public class PhoneBill extends AbstractPhoneBill{
+    @Override
+    public String getCustomer() {
+        return null;
+    }
+
+    @Override
+    public void addPhoneCall(AbstractPhoneCall abstractPhoneCall) {
+
+    }
+
+    @Override
+    public Collection<String> getPhoneCalls() {
+        return null;
+    }
+}
--- phonebill/src/main/java/edu/pdx/cs410J/ads6/Project1.java
+++ phonebill/src/main/java/edu/pdx/cs410J/ads6/Project1.java
@@ -7,6 +7,12 @@ public class Project1 {
 
   public static void main(String[] args) {
     PhoneCall call = new PhoneCall();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    if(args.length != 5){
+      missingArgs(args);
+    }
+  }
+
+  private static void missingArgs(String[] args){
     System.err.println("Missing command line arguments");
     for (String arg : args) {
       System.out.println(arg);
