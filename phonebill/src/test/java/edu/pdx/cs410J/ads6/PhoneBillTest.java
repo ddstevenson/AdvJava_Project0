44371c18d11faf4b009c20f11c72a15c9b530211
dds <11372904+ddstevenson@users.noreply.github.com>
2020-07-03 17:55:40 -0700

    Added PhoneBillTest class

---
+++ phonebill/src/test/java/edu/pdx/cs410J/ads6/PhoneBillTest.java
@@ -0,0 +1,9 @@
+package edu.pdx.cs410J.ads6;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PhoneBillTest {
+}
